1cceeadb3a9773a8dd624f1d81983a03be7587b5
sainath-kadam <mydearsainathkadam@gmail.com>
2024-02-14 23:00:50 +0530

    Backend  routes updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-// https://vitejs.dev/config/
+
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://estate1-uy7g.onrender.com/',
-        secure: false,
+        target: 'https://estate2.onrender.com',
+        secure: true, 
+        changeOrigin: true, 
       },
     },
   },
-
   plugins: [react()],
 });
